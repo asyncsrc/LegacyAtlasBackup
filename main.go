643a90dc6096387ee3a9a6f87f6e03c0ea8eabf8
Joseph Gimenez <joseph.gimenez@snagajob.com>
2018-05-19 11:03:54 -0400

    Improve error handling

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"log"
 	"time"
+	"errors"
 )
 
 var (
@@ -40,12 +41,14 @@ func getEnvironmentsForPage(pageNumber int) (map[string]int, error) {
 	environments := make(map[string]int)
 
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://app.terraform.io/ui/environments?enterprise_tool=terraform&page=%d&username=%s", pageNumber, organizationName), nil)
-
 	if err != nil {
-		return map[string]int{}, nil
+		return map[string]int{}, err
 	}
 
 	jsonResponse, err := getJSONFromRequest(req)
+	if err != nil {
+		return map[string]int{}, err
+	}
 
 	for _,v := range jsonResponse["environments"].([]interface{}) {
 		environments[v.(map[string]interface{})["name"].(string)] = int(v.(map[string]interface{})["current_state_id"].(float64))
@@ -56,15 +59,15 @@ func getEnvironmentsForPage(pageNumber int) (map[string]int, error) {
 
 func getLatestStateVersionForEnvironment(environmentName string, id int) (int, error) {
 	requestString := fmt.Sprintf("https://app.terraform.io/ui//%s/environments/%s/states/%d/state-versions?page=1", organizationName, environmentName, int(id))
+
 	req, err := http.NewRequest("GET", requestString, nil)
 	if err != nil {
 		return -1, err
 	}
 
 	jsonResponse, err := getJSONFromRequest(req)
-
 	if err != nil {
-		return -1, nil
+		return -1, err
 	}
 
 	if (len(jsonResponse["state_versions"].([]interface{})) > 0) {
@@ -72,7 +75,7 @@ func getLatestStateVersionForEnvironment(environmentName string, id int) (int, e
 		return int(latestStateVersion.(float64)), nil
 	}
 
-	return -1, nil
+	return -1, errors.New(fmt.Sprintf("No states found for environment: %s", environmentName))
 }
 
 func downloadState(environmentName string, id int, stateVersion int) error {
@@ -91,9 +94,13 @@ func downloadState(environmentName string, id int, stateVersion int) error {
 	}
 
 	req.Header.Set("Cookie", fmt.Sprintf("_atlas_session_data=%s", atlasSessionToken))
-	resp, err := client.Do(req)
-	data, err := ioutil.ReadAll(resp.Body)
 
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
@@ -115,13 +122,15 @@ func main() {
 	o := parser.String("o", "org", &argparse.Options{Required: true, Help: "Organization name"})
 
 	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
 	atlasSessionToken = *c
 	sessionDownloadPath = *p
 	organizationName = *o
 
-	if err != nil {
-		fmt.Print(parser.Usage(err))
-	}
 	for {
 		fmt.Println("Getting environments for page: ", pageNumber)
 		environments, err := getEnvironmentsForPage(pageNumber)
